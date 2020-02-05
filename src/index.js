73db3e0a197d2384e0d93af8e6a730a21d31aa6b
gyxman-main <42173229+gyxman-main@users.noreply.github.com>
2020-02-05 21:49:13 +0300

    add hw

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from "./components/app/app.jsx";
+
+const ERRORS_COUNT = 3;
+
+ReactDOM.render(
+    <App errorsCount={ERRORS_COUNT}>
+    </App>,
+    document.getElementById(`root`)
+);
