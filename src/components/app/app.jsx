a2d34b0c2bec8c26b95be35a96865fe3aacfe4ac
Roman Aleksyutin <gyxman@yandex.ru>
2020-02-17 21:25:39 +0300

    work

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PropTypes from "prop-types";
 import {WelcomeScreen} from "../welcome-screen/welcome-screen.jsx";
+import {ArtistQuestionScreen} from "../artist-question-screen/artist-question-screen.jsx";
+import {GenreQuestionScreen} from "../genre-question-screen/genre-question-screen.jsx";
 
-const welcomeButtonHandler = () => {};
-
-export const App = ({errorsCount}) => {
-  return <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={welcomeButtonHandler}/>;
+const welcomeButtonHandler = () => {
 };
+
+export const App = ({errorsCount, questions}) => {
+  // return <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={welcomeButtonHandler}/>;/**/
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={welcomeButtonHandler} />
+      </Route>
+      <Route exact path="/dev-artist">
+        <ArtistQuestionScreen question={questions[1]} />
+      </Route>
+      <Route exact path="/dev-genre">
+        <GenreQuestionScreen question={questions[0]} />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
+};
+
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
---
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -0,0 +1,65 @@
+import React, {PureComponent}  from "react";
+import PropTypes from "prop-types";
+
+export class GenreQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {question: {genre, answers}} = this.props;
+
+    return (
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+          </a>
+
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+                    style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
+          </svg>
+
+          <div className="game__mistakes">
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Выберите {genre} треки</h2>
+          <form className="game__tracks">
+            {answers.map((answer, i) => (
+              <div key={`${i}-${answer.genre}`} className="track">
+                <button className="track__button track__button--play" type="button"></button>
+                <div className="track__status">
+                  <audio></audio>
+                </div>
+                <div className="game__answer">
+                  <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`}
+                         id={`answer-${i}`} />
+                  <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
+                </div>
+              </div>
+            ))}
+
+            <button className="game__submit button" type="submit">Ответить</button>
+          </form>
+        </section>
+      </section>
+    )
+  }
+};
+
+GenreQuestionScreen.propTypes = {
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      genre: PropTypes.string.isRequired,
+    })).isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
